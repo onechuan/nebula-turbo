271d4266565e806473a48f4723255f603c56be71
ekawa <onechuan2020@163.com>
2024-06-05 23:23:41 +0800

    feat: 初始化项目

---
+++ apps/pixijs-lab/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang='ts' setup>
+import * as PIXI from "pixi.js";
+
+// 检查 PixiJS 版本
+console.log(PIXI.VERSION);
+
+</script>
+
+<style scoped lang='scss'>
+
+</style>
